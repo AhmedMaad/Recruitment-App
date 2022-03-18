c992d909d5e62b63a81480e3ffb7ac1522ddaa85
AhmedMaad <atef.ahmed1341@gmail.com>
2022-03-18 20:13:46 +0200

    Company Profile Part 3

--- app/src/main/java/com/maad/recruitment/company/CompanyProfileActivity.kt
+++ app/src/main/java/com/maad/recruitment/company/CompanyProfileActivity.kt
@@ -3,10 +3,13 @@ package com.maad.recruitment.company
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -50,8 +53,21 @@ class CompanyProfileActivity : AppCompatActivity() {
             .document(id)
             .get()
             .addOnSuccessListener {
-                company = it.toObject(CompanyModel::class.java)!!
-                title = "${company.name} Profile"
+                    company = it.toObject(CompanyModel::class.java) ?: CompanyModel()
+                if (company.name.isNotEmpty()){
+                    title = "${company.name} Profile"
+                    binding.nameEt.setText(company.name)
+                    binding.detailsEt.setText(company.details)
+                    binding.phoneEt.setText(company.number)
+                    binding.locationEt.setText(company.location)
+                    binding.tvEmail.text = company.email
+                    Glide
+                        .with(this)
+                        .load(company.image)
+                        .transition(DrawableTransitionOptions.withCrossFade(1000))
+                        .into(binding.profileIv)
+                }
+
                 /*if (company.getName() != null) nameET.setText(user.getName())
                 if (company.getAbout() != null) aboutET.setText(user.getAbout())
                 if (company.getPhone() != null) phoneET.setText(user.getPhone())
